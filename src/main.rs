73ec2e3c1c2a70b82a25e04d0fa3f167dae8cee6
kyaoi <aoistudy90@gmail.com>
2025-02-09 00:25:33 +0900

    wip: add TODO comment

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ fn main() -> Result<(), Error> {
     let args = Args::parse();
     let mut todos = load_todos();
 
+    // TODO:
+    // - すべてのTODOを削除するコマンド
+    // - 何もTODOがないときにはそれ用の表示をする
+    // - TODOを編集するコマンド
     match args.command {
         Some(Commands::Add) => add_todo_action(&mut todos)?,
         Some(Commands::List) => list_todos(&mut todos),
